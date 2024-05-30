5971f0d4c4ab01353b2f77e45ef04333bdff50b9
Khan Kibria <khan.kibria@gmail.com>
2024-05-30 01:38:35 -0700

    initial release

---
+++ src/addcmd/main.py
@@ -0,0 +1,69 @@
+import argparse
+import sys
+from warnings import warn
+from pathlib import Path
+from mkcmd.cli import build
+from tomlkit import toml_file, nl
+
+def do_main(path, cmdname, flag, **kwargs):
+    dst = Path(path)
+    cfg = dst.joinpath('pyproject.toml')
+    tomlfile = toml_file.TOMLFile(cfg.absolute())
+    config = tomlfile.read()
+
+    tp = config['tool']['poetry']
+    pkg = tp['packages'][0]
+    cmdpy = dst.joinpath(pkg['from'], pkg['include'], cmdname)
+
+    pyf = cmdpy.parent.joinpath(cmdpy.name + '.py')
+    if pyf.exists():
+        raise IOError(f'"{pyf.name}" already exists, exiting!')
+
+    build({}, cmdpy, False, False, **kwargs)
+
+    ins = 'scripts' not in tp
+    if ins:
+        tp['scripts'] = {}
+
+    tp['scripts'][cmdname] = f'{pkg["include"]}.{cmdname}:main'
+ 
+    if ins:
+        tp['scripts'].add(nl())
+
+    tomlfile.write(config)
+    pass
+
+def main():
+    params = {"app": "addcmd"}
+
+    parser = argparse.ArgumentParser(
+        prog=params["app"],
+        description='Creates python command file',
+        epilog=f'python -m {params["app"]}')
+
+    parser.add_argument('path')
+    parser.add_argument('cmdname')
+    parser.add_argument('-f', '--flag', default=False,
+                    action='store_true')
+
+    args = parser.parse_args()
+    params = params | vars(args)
+
+    set_warnigs_hook()
+    try:
+        do_main(**params)
+    except Exception as e:
+        print(f'{e.__class__.__name__}:', *e.args)
+        return 1
+    
+    return 0
+
+def set_warnigs_hook():
+    import sys
+    import warnings
+    def on_warn(message, category, filename, lineno, file=None, line=None):
+        print(f'Warning: {message}', file=sys.stderr)
+    warnings.showwarning = on_warn
+
+if __name__ == '__main__':
+    sys.exit(main())
